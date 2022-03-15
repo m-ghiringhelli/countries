e56edf3874bc578fddccc096eaf4ec16a68b7f32
m-ghiringhelli <mghiring@gmail.com>
2022-03-15 15:24:31 -0700

    added error message

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import CountryCard from '../../components/CountryCard/CountryCard';
 import { fetchCountries } from '../../services/countries';
+import './Main.css';
 
 export default function Main() {
   const [countries, setCountries] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const resp = await fetchCountries();
-      setCountries(resp);
+      try {
+        const resp = await fetchCountries();
+        setCountries(resp);
+      } catch (e) {
+        setErrorMessage('Uh-oh! Something is effed up. Try reloading the page? Or writing us an angry letter');
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <div>
-      {countries.map((country) => (
-        <CountryCard key={country.id} name={country.name} code={country.iso2} />
-      ))}
-    </div>
+    <>
+      <p>{errorMessage}</p>
+      <div className='country-list'>
+        {countries.map((country) => (
+          <CountryCard key={country.id} name={country.name} code={country.iso2} />
+        ))}
+      </div>
+    </>
   );
 }
