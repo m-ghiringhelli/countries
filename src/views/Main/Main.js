f6badf8de82d8dca88e6761b72c4462ccbb0776c
m-ghiringhelli <mghiring@gmail.com>
2022-03-15 14:51:49 -0700

    successfully fetches country array from supabase

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,8 +1,16 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import CountryCard from '../../components/CountryCard/CountryCard';
+import { fetchCountries } from '../../services/countries';
 
 export default function Main() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchCountries();
+      console.log(resp);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <CountryCard name={'South Africa'} code={'za'}/>
