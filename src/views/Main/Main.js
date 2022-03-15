15fa94d35769760c0a9af22edca80196cfffe35b
m-ghiringhelli <mghiring@gmail.com>
2022-03-14 17:36:15 -0700

    copied example flag

---
+++ src/views/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>
+      <img
+        src="https://flagcdn.com/w80/za.png"
+        srcSet="https://flagcdn.com/w160/za.png 2x"
+        width="80"
+        alt="South Africa"></img>
+    </div>
+  );
+}
